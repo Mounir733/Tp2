63ecdd61664b1bb2c46a90e6af83ffe43423895b
Mounir733 <107555755+Mounir733@users.noreply.github.com>
2023-01-30 17:39:20 +0100

    partie 2

---
+++ ClientConvertisseurV2/Models/Devise.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClientConvertisseurV2.Models
+{
+    public class Devise
+    {
+        private int id;
+        private string? nomdevise;
+        private double taux;
+
+        public Devise()
+        {
+        }
+
+        public Devise(int id, string? nomdevise, double taux)
+        {
+            this.Id = id;
+            this.NomDevise = nomdevise;
+            this.Taux = taux;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        [Required]
+        public string? NomDevise
+        {
+            get
+            {
+                return nomdevise;
+            }
+
+            set
+            {
+                nomdevise = value;
+            }
+        }
+
+        public double Taux
+        {
+            get
+            {
+                return taux;
+            }
+
+            set
+            {
+                taux = value;
+            }
+        }
+
+        
+    }
+}
--- ClientConvertisseurV2/ViewModels/ConvertisseurEuroViewModel.cs
+++ ClientConvertisseurV2/ViewModels/ConvertisseurEuroViewModel.cs
@@ -1,46 +1,33 @@
-// Copyright (c) Microsoft Corporation and Contributors.
-// Licensed under the MIT License.
-
-using ClientConvertisseurV1.Models;
-using ClientConvertisseurV1.Services;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Text;
+using System.Threading.Tasks;
+using ClientConvertisseurV2.Models;
+using ClientConvertisseurV2.Services;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml.Controls;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
-namespace ClientConvertisseurV1.Views
+namespace ClientConvertisseurV2.ViewModels
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class ConvertisseurEuroPage : Page, INotifyPropertyChanged
+    public class ConvertisseurEuroViewModel : ObservableObject, INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
         private double montant;
         private ObservableCollection<Devise> devises;
         private Devise deviseSelected;
         private double montantResult;
-        public ConvertisseurEuroPage()
-        {
-            this.InitializeComponent();
-            this.DataContext = this;
-            GetDataOnLoadAsync();
-        }
+        //public ConvertisseurEuroPage()
+        //{
+        //    this.InitializeComponent();
+        //    this.DataContext = this;
+        //    GetDataOnLoadAsync();
+        //}
+
+
 
         public double Montant
         {
@@ -56,7 +43,7 @@ namespace ClientConvertisseurV1.Views
             }
         }
 
-        
+
 
         public double MontantResult
         {
@@ -100,19 +87,19 @@ namespace ClientConvertisseurV1.Views
             }
         }
 
-        private async void GetDataOnLoadAsync()
+        public async void GetDataOnLoadAsync()
         {
-            WSService service = new WSService("https://localhost:7032/api/"); 
-            List<Devise> result = await service.GetDevisesAsync("devises"); 
+            WSService service = new WSService("https://localhost:7032/api/");
+            List<Devise> result = await service.GetDevisesAsync("devises");
             if (result == null)
             {
                 DisplayNoAPI();
             }
 
-            else   
+            else
                 Devises = new ObservableCollection<Devise>(result);
 
-            
+
         }
 
         protected void OnPropertyChanged(string name)
@@ -123,31 +110,43 @@ namespace ClientConvertisseurV1.Views
                 handler(this, new PropertyChangedEventArgs(name));
             }
         }
-        private void BtnConvertir_Click(object sender, RoutedEventArgs e)
+
+        public IRelayCommand BtnSetConversion { get; }
+        public ConvertisseurEuroViewModel()
         {
-            if (DeviseSelected == null)
+                //Boutons
+                BtnSetConversion = new RelayCommand(ActionSetConversion);
+            GetDataOnLoadAsync();
+
+        }
+        private void ActionSetConversion()
+        {
+            WSService service = new WSService("https://localhost:7032/api/");
+
+            if (deviseSelected == null)
+            { 
                 DisplayNoDevise();
+            }
             else
             {
-                WSService service = new WSService("https://localhost:7032/api/");
                 MontantResult = service.CalculMontant(DeviseSelected, Montant);
             }
         }
 
-        private async void DisplayNoDevise()
+        public async void DisplayNoDevise()
         {
             ContentDialog noDevise = new ContentDialog
             {
                 Title = "Erreur",
-                Content = "Vous n'avez pas selectionn� de devise.",
+                Content = "Vous n'avez pas selectionné de devise.",
                 CloseButtonText = "Ok"
             };
 
-            noDevise.XamlRoot = this.Content.XamlRoot;
+            noDevise.XamlRoot = App.MainRoot.XamlRoot;
             await noDevise.ShowAsync();
         }
-        private async void DisplayNoAPI()
-        { 
+        public async void DisplayNoAPI()
+        {
             ContentDialog noAPI = new ContentDialog
             {
                 Title = "Erreur",
@@ -155,7 +154,7 @@ namespace ClientConvertisseurV1.Views
                 CloseButtonText = "Ok"
             };
 
-            noAPI.XamlRoot = this.Content.XamlRoot;
+            noAPI.XamlRoot = App.MainRoot.XamlRoot;
             await noAPI.ShowAsync();
         }
     }
